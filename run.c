9ab001de7247140f401d83fd62065c1fbbc02114
Lee Jae Won <varyly@naver.com>
2017-12-22 18:34:04 +0900

    aaaa

--- run.c
+++ run.c
@@ -13,20 +13,43 @@ p_meta find_meta(p_meta *last, size_t size) {
   switch(fit_flag){
     case FIRST_FIT:
     {
+while(index){
+if(index->free=1&&index->=size)
+{  result=index;
+break;
+}
+index=index->next;
       //FIRST FIT CODE
     }
     break;
 
+
     case BEST_FIT:
     {
+while(index)
+{
+if(index->free=1&&index->size>=size&&((lastsize==-1)||(lastsize>index->size)))
+{
+lastsize=index->size;
+result=index;
+}
+index=index->next;
       //BEST_FIT CODE
     }
+}
     break;
 
     case WORST_FIT:
-    {
+    {while(index){
+if(index->free==1&&index->size>=size){if(best_size==-1||index->size>best_size){
+result=index;
+best_size=index->size;}
+}
+
+index=index->next;
       //WORST_FIT CODE
     }
+}
     break;
 
   }
